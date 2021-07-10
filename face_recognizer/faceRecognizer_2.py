cd7c0548b3a9fa1900eb414442096162e1f10bba
hualde <javhualde@gmail.com>
2021-07-10 10:33:11 +0200

    a

--- face_recognizer/faceRecognizer_2.py
+++ face_recognizer/faceRecognizer_2.py
@@ -14,13 +14,14 @@ Encoding = [seema_encoding, donald_encoding]
 Names = ['Seema', 'Donald Trampas']
 
 font = cv2.FONT_HERSHEY_SIMPLEX
-test_image = face_recognition.load_image_file('/home/hualde-jetson/p_m_w/opencv_tutorial/face_recognizer/demoImages/unknown/u9.jpg')
+test_image = face_recognition.load_image_file('/home/hualde-jetson/p_m_w/opencv_tutorial/face_recognizer/demoImages/unknown/u10.jpg')
 facePositions = face_recognition.face_locations(test_image)
 allEncodings = face_recognition.face_encodings(test_image, facePositions)
 
+test_image = cv2.cvtColor(test_image, cv2.COLOR_RGB2BGR)
+
 while(True):
     
-    test_image = cv2.cvtColor(test_image, cv2.COLOR_RGB2BGR)
 
     for (top, right, bottom, left), face_encoding in zip(facePositions, allEncodings):
         name = 'Unknow Person'
@@ -28,9 +29,9 @@ while(True):
         if True in matches:
             first_match_index = matches.index(True)
             name = Names[first_match_index]
-        cv2.rectangle(test_image,(left,top),(right,bottom),(0,0,255),2)
-        cv2.putText(test_image,name,(left,top-6),font,1,(255,0,255),1)
-        resized = cv2.resize(test_image, (480,320), interpolation = cv2.INTER_AREA)
+        cv2.rectangle(test_image,(left,top),(right,bottom),(0,0,255),4)
+        cv2.putText(test_image,name,(left,top-6),font,2,(255,0,255),3)
+        resized = cv2.resize(test_image, (640,480), interpolation = cv2.INTER_AREA)
         cv2.imshow('window', resized)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):    
